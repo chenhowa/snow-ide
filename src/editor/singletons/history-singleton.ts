b7249dbb144fd6f58842b8dff2a683a6971c0b82
Howard Chen <Howard.Chen@cognizant.com>
2019-02-08 13:05:58 -0700

    Add HistorySingleton implementation

--- src/editor/singletons/history-singleton.ts
+++ src/editor/singletons/history-singleton.ts
@@ -1,10 +1,14 @@
 
 import { History, CommandHistory } from "editor/editor_commands/command-history";
 
+var history: CommandHistory;
 
 var HistorySingleton = {
-    get: function() {
-
+    get: function(): History {
+        if(!history) {
+            history = new CommandHistory(15);
+        }
+        return history;
     }
 }
 

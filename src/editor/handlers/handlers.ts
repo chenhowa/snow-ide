f9f3b9f414faac1ea16e463cdafb13689a924f40
Howard Chen <Howard.Chen@cognizant.com>
2019-02-14 12:55:56 -0700

    Remove unnecessary files

--- src/editor/handlers/handlers.ts
+++ src/editor/handlers/handlers.ts
@@ -1,12 +1,10 @@
 
 
 import ClickHandler from "editor/handlers/click-handler";
-import MouseClickHandler from "editor/handlers/mouseclick-handler";
 import Handler from "editor/handlers/handler";
 
 
 export {
     Handler,
     ClickHandler,
-    MouseClickHandler
 };
